97ee3eef803ef0b994d6f36dc554bbcb865eb6bc
Robert <robert.kwiatkowski01@gmail.com>
2022-07-12 10:27:20 +0200

    First commit

---
+++ read_data.py
@@ -0,0 +1,23 @@
+def read_data(train=True, sample=False, cust_ratio=CFG.SAMPLE_RATIO):
+    """
+    Args:
+    train - bool - True if to read a train file
+    sample - bool - True if to draw a sample
+    cust_ratio - float - a ratio of customers to be sampled
+    """
+
+    if train:
+        FILE = 'train.parquet'
+    else:
+        FILE = 'test.parquet'
+
+    df = pd.read_parquet(DATA_DIR + FILE)
+
+    print(f"Database shape: {df.shape}")
+    if sample:
+        n_customers = df['customer_ID'].nunique()
+        no_of_cust = int(n_customers * cust_ratio)
+        cust_ids = np.random.choice(df['customer_ID'].unique(), no_of_cust)
+        df = df[df['customer_ID'].isin(cust_ids)]
+        print(f'Rows in sampled database: {df.shape[0]}')
+    return df
